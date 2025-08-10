dc2787113ef842a8c1b3ae910c2f2005a2c7e3d2
Keerthidharan.P <147246489+kproduce@users.noreply.github.com>
2025-08-10 12:21:46 +0530

    Add files via upload

---
+++ two_sums.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            k=target-nums[i]
+            if k in nums and nums.index(k)!=i:
+                return[i,nums.index(k)]
+                
